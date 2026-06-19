00c9689df329ff7577c4de8f9e322346b83fa6e7
umangakhanal <umanga.khanal@excelcollege.edu.np>
2026-06-19 13:52:22 +0545

    Payroll structure being made routes defined

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmployeeController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/employees/create',[EmployeeController::class,'create']);
