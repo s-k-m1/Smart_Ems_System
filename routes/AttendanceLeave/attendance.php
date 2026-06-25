276e6732875e7393a780fb3879839f5325baa515
Ranjit982 <panditranjit982@gmail.com>
2026-06-25 09:22:03 +0545

    Add leave module completely

--- routes/AttendanceLeave/attendance.php
+++ routes/AttendanceLeave/attendance.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AttendanceLeave\AttendanceDashboardController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AttendanceLeave\LeaveController;
 
 Route::get('/attendance', [AttendanceDashboardController::class, 'index']);
 Route::get('/attendance/create', [AttendanceDashboardController::class, 'create']);
@@ -12,3 +14,6 @@ Route::post('/attendance/{id}/update', [AttendanceDashboardController::class, 'u
 Route::get('/attendance/{id}/delete', [AttendanceDashboardController::class, 'destroy']);
 
 Route::get('/attendance/report', [AttendanceDashboardController::class, 'report']);
+
+Route::get('/leave', [LeaveController::class, 'index']);
+Route::post('/leave/store', [LeaveController::class,'store']);
