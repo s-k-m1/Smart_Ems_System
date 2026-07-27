9e58ab0b812c824928528c09a47480f200178dd6
s-k-m1 <skycse001@gmail.com>
2026-07-27 08:39:31 +0545

    clean: restore working password reset flow

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -17,25 +17,11 @@ class ForgotPasswordController extends Controller
 
     public function sendResetLinkEmail(Request $request)
     {
-        $request->validate(['email' => 'required|email']);
-
         $user = User::where('email', $request->email)->first();
-
-        if (!$user) {
-            return back()->withErrors(['email' => "We can't find a user with that email address."]);
-        }
 
         $token = Password::createToken($user);
 
-        \Illuminate\Support\Facades\Log::info('Starting email send for: ' . $user->email);
-
-        try {
-            set_time_limit(5);
-            $sent = $user->notify(new PasswordReset($token));
-            \Illuminate\Support\Facades\Log::info('Email sent successfully');
-        } catch (\Throwable $e) {
-            \Illuminate\Support\Facades\Log::error('Email failed: ' . get_class($e) . ': ' . $e->getMessage());
-        }
+        $user->notify(new PasswordReset($token));
 
         return back()->with(['status' => 'We have emailed your password reset link!']);
     }
